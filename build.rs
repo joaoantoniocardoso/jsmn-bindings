3c3cbcbc99c10428b32a8aa938175e8cd5b6db44
João Antônio Cardoso <joao.maker@gmail.com>
2025-04-23 00:04:44 -0300

    build.rs: Update bindgen API, fix feature-to-flag part

--- build.rs
+++ build.rs
@@ -5,39 +5,46 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
-
 fn main() {
+    // Only regenerate if jsmn changed
+    println!("cargo:rerun-if-changed=src/jsmn");
+
     // Build jsmn library, with optional compiler directives
     let mut build = cc::Build::new();
-    let build = build.file("src/jsmn/jsmn.c");
-    let build = build.include("src/jsmn");
+    let mut builder = bindgen::Builder::default();
 
-    #[cfg(feature = "parent-links")]
-    let mut build = build.define(parent_links, None);
+    if std::env::var_os("CARGO_FEATURE_PARENT_LINKS").is_some() {
+        println!("cargo:rustc-cfg=feature=\"parent-links\"");
+        build.define("JSMN_PARENT_LINKS", None);
+        builder = builder.clang_arg("-DJSMN_PARENT_LINKS");
+    }
 
-    #[cfg(feature = "strict")]
-    let mut build = build.define(strict, None);
+    if std::env::var_os("CARGO_FEATURE_STRICT").is_some() {
+        println!("cargo:rustc-cfg=feature=\"strict\"");
+        build.define("JSMN_STRICT", None);
+        builder = builder.clang_arg("-DJSMN_STRICT");
+    }
 
-    build.compile("jsmn");
-
+    build
+        .file("src/jsmn/jsmn.c")
+        .include("src/jsmn")
+        .compile("jsmn");
 
     // Generate bindings for jsmn
-    let bindings = bindgen::Builder::default()
+    let bindings = builder
         .header("src/jsmn/jsmn.h")
-        .whitelisted_type("jsmntype_t")
-        .whitelisted_type("jsmnerr")
-        .whitelisted_type("jsmntok_t")
-        .whitelisted_type("jsmn_parser")
-        .whitelisted_function("jsmn_init")
-        .whitelisted_function("jsmn_parse")
+        .allowlist_type("jsmntype_t")
+        .allowlist_type("jsmnerr")
+        .allowlist_type("jsmntok_t")
+        .allowlist_type("jsmn_parser")
+        .allowlist_function("jsmn_init")
+        .allowlist_function("jsmn_parse")
         .generate()
         .expect("Unable to generate bindings!");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    bindings.write_to_file(out_path.join("bindings.rs"))
-            .expect("Couldn't write bindings.rs!");
-
-    // Only regenerate if jsmn changed
-    println!("cargo:rerun-if-changed=src/jsmn");
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings.rs!");
 }
